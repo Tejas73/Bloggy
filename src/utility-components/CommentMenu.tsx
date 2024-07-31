eff2c1c03a3728c34d8a07ef36ceb99323e2f677
TejasBHOYAR <tejasbhoyar5@gmail.com>
2024-07-31 17:14:05 +0530

    fixed the display of incorrect comment menu for all the comments

--- src/utility-components/CommentMenu.tsx
+++ src/utility-components/CommentMenu.tsx
@@ -49,7 +49,7 @@ const CommentMenu: React.FC<CommentMenuProps> = ({
         }
         getCurrUserId();
     }, []);
-    
+
     const handleClickEditComment = async () => {
         setSelectedCommentId(id);
         setIsEditComment(true);
@@ -126,103 +126,4 @@ const CommentMenu: React.FC<CommentMenuProps> = ({
 };
 
 export default CommentMenu;
-
-
-// import React, { useEffect, useId } from 'react';
-// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-// import useAuth from '@/hooks/useAuth';
-// import { currUserId, isLoggedIn } from '@/store/atoms/isLoggedIn';
-// import axios from 'axios';
-// import { useRecoilState, useRecoilValue, useRecoilValueLoadable, useSetRecoilState } from 'recoil';
-// import { currCommentState, editCommentState, selectedCommentIdState } from '@/store/atoms/commentAtoms';
-// import { currUserIdSelector } from '@/store/selectors/currUserIdSelector';
-// import { useCookies } from 'react-cookie';
-
-// // the menu button svg is displayed on incorrect comment. it only displays correctly after refresh
-
-// interface CommentMenuProps {
-//     userId: string;
-//     id: string;
-// }
-
-// const CommentMenu: React.FC<CommentMenuProps> = ({ id, userId }) => {
-//     useAuth();
-//     const authState = useRecoilValue(isLoggedIn);
-//     const setSelectedCommentId = useSetRecoilState(selectedCommentIdState);
-//     const [comments, setComments] = useRecoilState(currCommentState);
-//     const setIsEditComment = useSetRecoilState(editCommentState);
-//     const authUserIdLoadable = useRecoilValueLoadable(currUserIdSelector);
-//     const setAuthUserId = useSetRecoilState(currUserId);
-//     const [cookie, setCookie] = useCookies(['jwt']);
-
-//     useEffect(() => {
-//         if (authUserIdLoadable.state === 'hasValue') {
-//             setAuthUserId(authUserIdLoadable.contents);
-//         }
-//     }, [authUserIdLoadable.state]);
-
-//     const handleClickEditComment = () => {
-//         setSelectedCommentId(id);
-//         setIsEditComment(true);
-//     };
-
-//     const handleDeleteComment = async () => {
-//         try {
-//             await axios.delete(`http://localhost:3000/api/comment/deletecomment/?userId=${userId}&id=${id}`);
-//             const updatedComments = comments.filter(comment => comment.id !== id);
-//             setComments(updatedComments);
-//             setSelectedCommentId(null);
-//         } catch (error) {
-//             console.error('Error deleting comment: ', error);
-//         }
-//     };
-//     console.log("userId in COMMENTMENU : ", userId)
-//     console.log("COMMENTMENU userid: ", authUserIdLoadable.contents?.userID)
-
-//     return (
-//         <div>
-//             {/* authState.isAuthenticated && userId === authUserId.userID */}
-//             {authState.isAuthenticated && userId === authUserIdLoadable.contents?.userID && (
-//                 <div className="flex items-center w-64">
-//                     <DropdownMenu>
-//                         <DropdownMenuTrigger>
-//                             <svg
-//                                 xmlns="http://www.w3.org/2000/svg"
-//                                 fill="none"
-//                                 viewBox="0 0 24 24"
-//                                 strokeWidth="1.5"
-//                                 stroke="currentColor"
-//                                 className="size-6"
-//                             >
-//                                 <path
-//                                     strokeLinecap="round"
-//                                     strokeLinejoin="round"
-//                                     d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-//                                 />
-//                             </svg>
-//                         </DropdownMenuTrigger>
-
-//                         <DropdownMenuContent >
-//                             <DropdownMenuItem onClick={handleClickEditComment}>
-//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-//                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-//                                 </svg>
-//                                 Edit
-//                             </DropdownMenuItem>
-
-//                             <DropdownMenuItem onClick={handleDeleteComment}>
-//                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-//                                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-//                                 </svg>
-//                                 Delete
-//                             </DropdownMenuItem>
-//                         </DropdownMenuContent>
-//                     </DropdownMenu>
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default CommentMenu;
 
